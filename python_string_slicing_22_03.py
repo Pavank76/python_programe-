3ee6618ff1317a33a6f4501203890a6b3b942728
Pavank76 <102588881+Pavank76@users.noreply.github.com>
2022-03-29 15:50:55 +0530

    class 22_03

---
+++ python_string_slicing_22_03.py
@@ -0,0 +1,20 @@
+##var='pavan'
+##print(var[0])
+##print(var[-5])
+
+##var="dhoni is india caption"
+##print(var[1])
+##print(var[0:])
+##print(var[0:5])
+##print(var[0:10])
+##print(var[0:15])
+##print(var[0:])
+
+##print(var[6:2])
+
+dot='data'
+print(dot)
+
+data='sales of the last week'
+print(slice,data(9))
+
